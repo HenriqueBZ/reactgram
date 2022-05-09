028bde14105140ebb26e8bd0d6449d9d6eb66b56
Henrique Zanini <hiquebzanini@gmail.com>
2022-05-09 20:25:47 -0300

    service e slice de login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -26,9 +26,32 @@ const logout = () => {
     localStorage.removeItem("user")
 }
 
+// Sign an user
+const login = async (data) => {
+
+    const config = requestConfig("POST", data)
+
+    try {
+
+        const res = await fetch(api + "user/login", config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+        if (res) {
+            localStorage.setItem("user", JSON.stringify(res))
+        }
+
+        return res
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const authService = {
     register,
     logout,
+    login,
 }
 
 export default authService
