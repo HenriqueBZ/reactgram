1b2826e91f22323451070cd26ace00a750ac439f
Henrique Zanini <hiquebzanini@gmail.com>
2022-05-19 13:01:58 -0300

    finalizando funcao de excluir foto

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -14,7 +14,12 @@ import { useParams } from "react-router-dom"
 
 // Redux
 import { getUserDetails } from "../../slices/userSlice"
-import { publishPhoto, resetMessage, getUserPhotos } from "../../slices/photoSlice"
+import {
+    publishPhoto,
+    resetMessage,
+    getUserPhotos,
+    deletePhoto,    
+} from "../../slices/photoSlice"
 
 const Profile = () => {
 
@@ -53,6 +58,12 @@ const Profile = () => {
         setImage(image)
     }
 
+    const resetComponentMessage = () => {
+        setTimeout(() => {
+            dispatch(resetMessage())
+        }, 2000)
+    }
+
     const submitHandle = (e) => {
         e.preventDefault()
 
@@ -74,9 +85,15 @@ const Profile = () => {
 
         setTitle("")
 
-        setTimeout(() => {
-            dispatch(resetMessage)
-        }, 2000);
+        resetComponentMessage()
+    }
+
+    // Delete a photo
+    const handleDelete = (id) => {
+
+        dispatch(deletePhoto(id))
+
+        resetComponentMessage()
     }
 
     if(loading) {
@@ -138,7 +155,7 @@ const Profile = () => {
                                         <BsFillEyeFill />
                                     </Link>
                                     <BsPencilFill />
-                                    <BsXLg />
+                                    <BsXLg onClick={() => handleDelete(photo._id)}/>
                                 </div>
                             ) : (
                                 <Link className="btn" to={`/photos/${photo._id}`}>
