c566c44c0b98ab456c79e1407185348f5d111e7c
Henrique Zanini <hiquebzanini@gmail.com>
2022-05-26 19:21:45 -0300

    corrigindo rotas

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -53,6 +53,7 @@ const deletePhoto = async(req,res) => {
         // Check if photo belongs to user
         if (!photo.userId.equals(reqUser._id)) {
             res.status(422).json({ errors: ["Ocorreu um erro, tente novamente mais tarde."]})
+            return
         }
 
         await Photo.findByIdAndDelete(photo._id)
